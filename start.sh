f459db255a825bf4e8ed8e8cc61e5128a3e63137
kikkaras <115210577+kikkaras@users.noreply.github.com>
2022-12-25 12:43:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/kikkaras/urluploadbot.git /urluploadbot
+cd /urluploadbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
